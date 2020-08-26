03f4159f4681d8ca49e3867e09c17e97d803a682
Abby Allan <abbyallan306@gmail.com>
2020-08-26 09:56:20 -0600

    added js name fill

--- joesbb/main.js
+++ joesbb/main.js
@@ -1,3 +1,11 @@
 document.getElementById('company').innerHTML = "Joe's Bed & Breakfast"
 
 document.querySelector('header > h2').innerText = "Feels Like Home"
+
+let userName = prompt("What is your name?")
+
+//let message = "Hello " + userName + ", welcome to the best lodging in Utah!"
+
+let message = `Hello ${userName}, welcome to the finest lodging in Utah County`
+
+document.querySelector('#greeting').innerText = message
