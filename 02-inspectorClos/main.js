dcbf7873635b25eac5510f0082e44deebb5bb4b3
Abby Allan <abbyallan306@gmail.com>
2020-08-31 11:31:50 -0600

    constant change to reDisplay

--- 02-inspectorClos/main.js
+++ 02-inspectorClos/main.js
@@ -2,8 +2,10 @@ document.getElementById('company').innerHTML = "Inspector Clouseau"
 
 document.querySelector('header > h2').innerText = "Cheif Inspector Clouseau Speaking on the Pheaun"
 
-
+function reDisplay() {
 let windowSizes = `Window Size is ${window.innerWidth} pixels wide by ${window.innerHeight} pixels tall.`
 let offset = `Window offset is ${window.screenX} from the left edge and ${window.screenY} pixels from the top of the display`
 
 let myWindow = document.querySelector('#myWindow').innerText = windowSizes + '\n' + offset
+}
+reDisplay()
