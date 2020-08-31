35bc9e1b8cfa92f15aee5c1330c946e6c2184024
Abby Allan <abbyallan306@gmail.com>
2020-08-31 12:31:15 -0600

    assignment complete

--- 02-inspectorClos/main.js
+++ 02-inspectorClos/main.js
@@ -5,7 +5,14 @@ document.querySelector('header > h2').innerText = "Cheif Inspector Clouseau Spea
 function reDisplay() {
 let windowSizes = `Window Size is ${window.innerWidth} pixels wide by ${window.innerHeight} pixels tall.`
 let offset = `Window offset is ${window.screenX} from the left edge and ${window.screenY} pixels from the top of the display`
+let url = `The URL for this page is ${window.location}`
 
-let myWindow = document.querySelector('#myWindow').innerText = windowSizes + '\n' + offset
+document.querySelector('#myWindow').innerText = `${windowSizes} \n ${offset} \n ${url}`
+
+
+let docTitle = `Document title is ${document.title}`
+let modified = `This document was last updated on ${document.lastModified}`
+
+document.querySelector('#myDocument').innerText = `${docTitle} \n ${modified}`
 }
-reDisplay()
+reDisplay() 
